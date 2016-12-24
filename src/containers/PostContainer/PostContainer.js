284c1fdf92a4a26ee8a69dd260b1c6b5f7a6ee02
somdari <matthewchoi7@gmail.com>
2016-12-25 08:19:03 +1100

    Creating Post component

--- src/containers/PostContainer/PostContainer.js
+++ src/containers/PostContainer/PostContainer.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { PostWrapper, Navigate } from '../../components';
+import { PostWrapper, Navigate, Post } from '../../components';
 
 class PostContainer extends React.Component {
     render() {
         return (
             <PostWrapper>
                 <Navigate/>
+                <Post/>
             </PostWrapper>
         );
     }
