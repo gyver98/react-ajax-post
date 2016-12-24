2b1612adce6a805827c4b8fcfead1b6bf5aed6dd
somdari <matthewchoi7@gmail.com>
2016-12-25 08:30:35 +1100

    Creating CommentList component

---
+++ src/components/CommentList/CommentList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './CommentList.css';
+
+const CommentList = () => (
+    <ul className="CommentList">
+    </ul>
+);
+
+export default CommentList;
--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Post.css';
+import { CommentList } from '../';
 
 const Post = () => (
     <div className="Post">
         <h1>Title</h1>
         <p>Body</p>
+        <CommentList/>
     </div>
 );
 
