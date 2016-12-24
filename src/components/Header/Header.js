18d0dc33c9fdcb1c7988392f34a565b8ab342071
somdari <matthewchoi7@gmail.com>
2016-12-25 06:59:26 +1100

    Creating Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Header.css';
+
+// function Header () {
+//     return (
+//         <div className="Header">
+//             POSTS
+//         </div>
+//     )
+// }
+
+// ES6 version
+const Header = () => (
+    <div className="Header">
+        POSTS
+    </div>
+)
+
+export default Header;
